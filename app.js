7b840de6f9f873dfb49de09d799982dc507e7546
ipyiywy <ipyiywy@gmail.com>
2021-08-24 17:51:46 +0800

    add detail page

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const port = 3000
-const restaurantsList = require('./restaurants.json')
-const RestautantSchema = require('./models/restaurant')
 
+const Restaurant = require('./models/restaurant')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -21,22 +21,60 @@ db.once('open', () => {
     console.log('mongodb connected!')
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
+//show all the data in index page
 app.get('/', (req, res) => {
-res.render('index', { restaurants: restaurantsList.results })
+    Restaurant.find()
+    .lean()
+    .then(restaurant => res.render('index', {restaurant}))
+    .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-const restaurant = restaurantsList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-res.render('details', { restaurant: restaurant })
+//CREATE FUNCTION
+//Create new page route
+app.get('/restaurant/new', (req, res) => {
+    return res.render('new')
 })
 
-app.get('/search', (req, res) => {
-const keyword = req.query.keyword
-const restaurants = restaurantsList.results.filter(restaurant => { return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase())})
-res.render('index', {restaurants: restaurants, keyword: keyword })
+//Create new item in database
+app.post('/restaurant', (req, res) => {
+    const name = req.body.name
+    const name_en = req.body.name_en
+    const category = req.body.category
+    const image = req.body.image
+    const location = req.body.location
+    const phone = req.body.phone
+    const google_map  = req.body.google_map
+    const rating  = req.body.rating
+    const description  = req.body.description
+
+    return Restaurant.create({name,
+    name_en,
+    category,
+    image, 
+    location,
+    phone,
+    google_map,
+    rating,
+    description})
+    .then(() => res.redirect('/'))
+.catch(error => console.log(error))
 })
+
+//Create detail page route
+app.get('/restaurant/detail/:id', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('detail', { restaurant: restaurant }))
+    .catch((error) => console.log(error))
+    })
+
+//EDIT FUNCTION
+
+
 
 
 app.listen(port, () => {
