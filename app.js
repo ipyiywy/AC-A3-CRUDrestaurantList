b6b3a23921b2b691e786afee1e5aa86537089d49
ipyiywy <ipyiywy@gmail.com>
2021-08-25 15:54:15 +0800

    added search function

--- app.js
+++ app.js
@@ -68,13 +68,70 @@ app.get('/restaurant/detail/:id', (req, res) => {
     const id = req.params.id
     return Restaurant.findById(id)
     .lean()
-    .then(restaurant => res.render('detail', { restaurant: restaurant }))
+    .then(restaurant => res.render('detail', { restaurant }))
     .catch((error) => console.log(error))
     })
 
-//EDIT FUNCTION
+//route to edit page
+app.get('/restaurant/detail/:id/edit', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+        .lean()
+        .then(restaurant => res.render('edit', { restaurant }))
+        .catch((error) => console.log(error))
+})
 
+//Update function
+app.post('/restaurant/detail/:id/edit', (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    const name_en = req.body.name_en
+    const category = req.body.category
+    const image = req.body.image
+    const location = req.body.location
+    const phone = req.body.phone
+    const google_map  = req.body.google_map
+    const rating  = req.body.rating
+    const description  = req.body.description
 
+    return Restaurant.findById(id)
+    .then(restaurant => {
+        restaurant.name = name
+        restaurant.name_en = name_en
+        restaurant.category = category
+        restaurant.image = image
+        restaurant.location = location
+        restaurant.phone = phone
+        restaurant.google_map = google_map
+        restaurant.rating = rating
+        restaurant.description = description
+        return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurant/detail/${id}`))
+    .catch(error => console.log(error))
+})
+
+//Delete function
+app.post('/restaurant/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+//Search function 
+app.get('/search', (req, res) => {
+const keyword = req.query.keyword
+Restaurant.find()
+    .lean()
+    .then( item => { 
+        const allitems = item.filter(restaurant => { 
+            return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())})
+        res.render('index', {restaurant: allitems, keyword: keyword })
+})
+})
+    
 
 
 app.listen(port, () => {
